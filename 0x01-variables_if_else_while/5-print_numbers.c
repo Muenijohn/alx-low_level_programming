a4d80a6b4e21297b60e9ac2d03c407e16c5eb94b
Muenijohn <pmueni592@gmail.com>
2022-07-01 08:39:19 -0700

    pauline

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints values
+ *
+ * Return: 0 shows success
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+	return (0);
+}
