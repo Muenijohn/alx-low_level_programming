b087758e162618891b251b98e429017367faed3b
Muenijohn <pmueni592@gmail.com>
2022-07-01 10:31:29 -0700

    pauline

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+
+{
+
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if (j > i)
+			{
+			putchar(i);
+			putchar(j);
+			if (i != 56)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
