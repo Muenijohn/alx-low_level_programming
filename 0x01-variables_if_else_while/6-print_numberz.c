a1cdd790ffbef4a3eb3ae9e84cca3a8d9e5833a2
Muenijohn <pmueni592@gmail.com>
2022-07-01 08:45:26 -0700

    pauline

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the results
+ *
+ * Return: 0 shows success
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
