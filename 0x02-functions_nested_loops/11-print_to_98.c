04c12264d01425f284630dade886cc17bf89a119
Muenijohn <pmueni592@gmail.com>
2022-07-05 07:06:29 -0700

    loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,14 @@
 #include "6-abs.c"
 #include <stdio.h>
 
+/**
+ * print_to_98 - prints all natural numbers
+ * from n to 98
+ *
+ * @n: input number
+ *
+ * Return:no return
+ */
 void print_to_98(int n)
 {
 	int i;
