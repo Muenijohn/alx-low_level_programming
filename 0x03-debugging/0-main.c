efcaaec99e5f236ee03728afcf43513b4deecf46
Muenijohn <pmueni592@gmail.com>
2022-07-06 12:28:03 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+carrie@ubuntu:/debugging$ cat main.c
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
+carrie@ubuntu:/debugging$
