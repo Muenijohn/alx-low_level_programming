e7c6b4225a2bbba9b59756790636ae501bc0f0cb
Muenijohn <pmueni592@gmail.com>
2022-07-18 04:47:21 -0700

    advanced

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 pointers,arrays and strings
+Even more pointers,arrays and strings
--- main.h
+++ main.h
@@ -1,11 +1,4 @@
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
+#ifndef _main.h_
+#define _main.h_
+
+char *_memset(char *s, char b, unsigned int n);
