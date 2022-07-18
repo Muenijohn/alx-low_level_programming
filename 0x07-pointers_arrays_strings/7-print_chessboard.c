a7900d500ea625c523119b1cc9a80fc39281f1e4
Muenijohn <pmueni592@gmail.com>
2022-07-18 06:38:26 -0700

    advanced

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,9 +14,9 @@ void print_chessboard(char (*a)[8])
 		if (i % 8 == 0 && i != 0)
 		{
 			m = i;
-			_putchar('\n');
+			putchar('\n');
 		}
-		_putchar(a[i / 8][i - m]);
+		putchar(a[i / 8][i - m]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
