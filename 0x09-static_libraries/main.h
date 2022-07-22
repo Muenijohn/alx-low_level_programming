76c10f816c4bb8ede6702f81e2378050d0792892
Muenijohn <pmueni592@gmail.com>
2022-07-22 13:23:55 -0700

    task

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,7 @@
-#ifndef _MAIN_H
 #define _MAIN_H
+
 #include <string.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +22,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif
