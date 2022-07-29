bf006ae324aa3d4813263b2d3fcd0f896fe12808
Muenijohn <pmueni592@gmail.com>
2022-07-29 02:34:08 -0700

    malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -30,5 +30,16 @@ void *malloc_checked(unsigned int b);
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+/**
+ * _realloc - allocate memory and set all values to 0
+ * @ptr: pointer to the memory previously allocated (malloc(old_size))
+ * @old_size: size previously allocated
+ * @new_size: new size to reallocate
+ *
+ * Return: pointer to reallocated memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 
 #endif
