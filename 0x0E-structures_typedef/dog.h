3eb1e274109f6bce8e9b90d3f95e59f54e5ca113
Muenijohn <pmueni592@gmail.com>
2022-08-01 07:34:42 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,30 +1,24 @@
-#ifndef _DOG_H
-#define _DOG_H
-
-#define NIL "(nil)"
-
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - structure for dogs
- * @name: first member
- * @age: second member
- * @owner: third member
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
