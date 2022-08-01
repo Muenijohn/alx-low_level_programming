d745b15539ad0e0752d5b53ee8719c2717b839ca
Muenijohn <pmueni592@gmail.com>
2022-08-01 07:15:11 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+#define NIL "(nil)"
+
+#include <stdio.h>
+
+/**
+ * struct dog - structure for dogs
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+#endif
