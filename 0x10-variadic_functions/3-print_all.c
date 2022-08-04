aba714074ee0bfde80365ac69dfdaae14f375736
Muenijohn <pmueni592@gmail.com>
2022-08-04 06:56:05 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,83 +1,85 @@
 #include "variadic_functions.h"
-
-/**
- * print_all - prints anything
- * @format: list of types of args
- *
- * Return: void
- */
-
-void print_all(const char * const format, ...)
-{
-	opt_t print[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-		{'\0', NULL}
-	};
-
-	va_list params;
-	int i, j;
-	char *sep = "";
-
-	va_start(params, format);
-	i = 0;
-	while (format[i])
-	{
-		j = 0;
-		while (print[j].opt)
-		{
-			if (print[j].opt == format[i])
-			{
-				printf("%s", sep);
-				print[j].meth(params);
-				sep = ", ";
-			}
-			j++;
-		}
-		i++;
-	}
-
-	va_end(params);
-	printf("\n");
-}
-
-/**
- * print_char - prints char
- * @params: params
- */
-void print_char(va_list params)
-{
-	printf("%c", va_arg(params, int));
-}
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_int - prints int
- * @params: params
+ * @list: arguments from print_all
  */
-void print_int(va_list params)
+void print_int(va_list list)
 {
-	printf("%d", va_arg(params, int));
+	printf("%d", va_arg(list, int));
 }
 
 /**
  * print_float - prints float
- * @params: params
+ * @list: arguments from print_all
  */
-void print_float(va_list params)
+void print_float(va_list list)
 {
-	printf("%f", va_arg(params, double));
+	printf("%f", va_arg(list, double));
 }
 
 /**
- * print_string - prints string
- * @params: params
+ * print_char - prints int
+ * @list: arguments from print_all
  */
-void print_string(va_list params)
+void print_char(va_list list)
 {
-	char *s;
+	printf("%c", va_arg(list, int));
+}
 
-	s = va_arg(params, char *);
-	printf("%s", s ? s : NIL);
+/**
+ * print_str - prints string
+ * @list: arguments from print_all
+ */
+void print_str(va_list list)
+{
+	char *s = va_arg(list, char *);
+
+	s == NULL ? printf("(nil)") : printf("%s", s);
+
+}
+
+/**
+ * print_all - prints any type
+ * @format: arguments to print
+ */
+
+void print_all(const char * const format, ...)
+{
+va_list list;
+int i = 0, j = 0;
+char *sep = "";
+
+printTypeStruct printType[] = {
+	{ "i", print_int },
+	{ "f", print_float },
+	{ "c", print_char },
+	{ "s", print_str },
+	{NULL, NULL}
+};
+
+
+va_start(list, format);
+
+while (format && format[i])
+{
+	j = 0;
+	while (j < 4)
+	{
+		if (*printType[j].type == format[i])
+		{
+			printf("%s", sep);
+			printType[j].printer(list);
+			sep = ", ";
+			break;
+		}
+		j++;
+	}
+	i++;
+}
+
+printf("\n");
+va_end(list);
 }
