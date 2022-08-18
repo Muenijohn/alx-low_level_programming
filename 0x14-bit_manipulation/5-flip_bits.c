e857defbc0b57ee7c2b5fb254a598188ab0a5736
Muenijohn <pmueni592@gmail.com>
2022-08-18 13:41:07 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,9 +9,6 @@
  * @n: the first bit word
  *
  * @m: the second bit word
- *
- *
- *
  * Return: the hamming distance
  *
  */
@@ -20,33 +17,8 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int shift = (sizeof(n) * BYTE_LENGHT);
 	int dist = 0;
+
 	while (shift--)
-
-		dist += (n >> shift & 1) != (m >> shift & 1);
-	return (dist);
-}
-
-/**
- * flip_bits - returns hamming distance of two bit words
- *
- * which is the number of bits you would need to flip to get
- *
- * from one number to another
- *
- * @n: the first bit word
- *
- * @m: the second bit word
- *
- *
- * Return: the hamming distance
- */
-
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{
-	int shift = (sizeof(n) * BYTE_LENGHT);
-	int dist = 0;
-	while (shift--)
-
 		dist += (n >> shift & 1) != (m >> shift & 1);
 	return (dist);
 }
